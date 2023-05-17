ab88a215132775b1a5e7a76d3e01f64ce7698566
Rollo Tully <rollotully@gmail.com>
2023-05-17 19:01:02 +0100

    big update

--- LBM.py
+++ LBM.py
@@ -1,9 +1,15 @@
 import numpy as np
-class LBM():
-    def __init__(self, foil):
-        self.Foil_address = "C:\Users\rollo\Documents\GitHub\Generative-Slicer\Airfoils\s1225_norm.dat"
-        self.foil = self.Load_foil()
-        self.mainloop()
+import cv2
+class Foil():
+    def __init__(self, address_, chord_):
+        self.foil_address = address_
+        self.Chord = chord_
+        self.Foil = self.Generate_Foil()
+        self.thickness = self.Find_thickness()
+        print(self.thickness)
+    def Find_thickness(self):
+        """Finds the thickness of the current foil"""
+        return (np.max(self.Foil[:,1])-np.min(self.Foil[:,1]))*self.Chord
 
     def format_dat(self,data):
         # TODO: Needs Improvment for greater compatibility
@@ -12,23 +18,70 @@ class LBM():
         self.formatted  = [[float(self.num) for self.num in list(filter(lambda x:x!='',self.coord))]for self.coord in self.formatted]#list(map(float,self.formatted))
         self.formatted = list(filter(lambda x:x!=[],self.formatted))
         return self.formatted
-
     def Load_foil(self):
         """Load the Airfoil for testing"""
         self.raw = open(self.foil_address,'r').read()
         self.foil_dat = np.array(self.format_dat(self.raw))[2:-2]
         return self.foil_dat
+    def Generate_Foil(self):
+        return self.Load_foil()*self.Chord
+    def Bounding_box(self):
+        return np.array([np.max(self.Foil[:,0])-np.min(self.Foil[:,0]),
+                         np.max(self.Foil[:,1])-np.min(self.Foil[:,1])])
+
+class Mesh_Element():
+    def __init__(self):
+        self.Flow_State = np.array([0,0])
+        self.Density = 1.
+class Source_Element():
+    """Defines the Inlet face"""
+    """This has a fixed flow condition and will not vary"""
+    def __init__(self):
+        self.Flow_State = np.array([1,0])
+class Surface_Element():
+    """Defines the surface of the test element"""
+    def __init__(self):
+
+class Sink_Element():
+    """Defines the outlet face"""
+    def __init__(self):
+
+class LBM():
+    def __init__(self):
+        self.Foil_address = "C:/Users/rollo/Documents/GitHub/Generative-Slicer/Airfoils/s1225_norm.dat"
+        self.foil = Foil(self.Foil_address,0.2)
+        self.element_size = 0.001 #Meters, so 1mm
+        self.mainloop()
+
     def Generate_Foil_Mask(self):
         """Generates a logical mesh of the airfoil"""
+        self.Foil_Dimensions = self.foil.Bounding_box()/self.element_size
+        self.Mesh_Dimensions = np.int32(self.Foil_Dimensions*np.array([4,8]))
+        self.foil_mask = np.zeros(tuple(self.Mesh_Dimensions[::-1]))#Creates a 0 initialised array
+        self.Foil_copy = self.foil.Foil*(1/self.element_size)
+        self.offset = np.array([self.Mesh_Dimensions/2])-self.foil.Bounding_box()/(2*self.element_size)
+        self.Foil_copy = self.Foil_copy+self.offset
+        self.Foil_copy = np.int32([self.Foil_copy])
+        self.foil_mask = cv2.fillPoly(self.foil_mask,self.Foil_copy,color=255)
+        self.foil_mask = (self.foil_mask>0)
+        #self.foil_mask = (self.foil_mask>125)*1
+        cv2.imshow("Foil", self.foil_mask)
+        cv2.waitKey(2000)
     def Generate_Lattice(self):
         """Creates the LBM lattice"""
+        pass
+
     def Streaming_step(self):
         """Defines the LBM streaming step"""
+        pass
     def Collision_step(self):
         """Defines the LBM collision step"""
+        pass
 
 
-    def mainloop():
+    def mainloop(self):
+        self.Generate_Foil_Mask()
+        pass
 
 if __name__ == "__main__":
     LBM()
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ without having to do extensive design work
 
 import numpy as np
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 class CFD():
     """This is an implementation of the Lattice Boltzmann simulation"""
     def __init__(self, Density_, Kinematic_Viscocity_, V_inf_, foil_):
@@ -16,13 +18,81 @@ class CFD():
         self.foil = foil_
 
 class Wing(Foil):
-    def __init__(self, root_, tip_, Sweep_, Dihedral_, ):
-        Foil.__init__()
-        self.Foils =
+    def __init__(self, surface_, y_, chord_, sweep_, dihedral_):
+        self.Sections = [Foil(surface_[i], chord_[i]) for i in range(len(chord_))]
+        self.Section_angles = [[sweep_[i], dihedral_[i], y_[i]] for i in range(len(chord_))] #Sweep angle, dihedral angle, and the distance from the plater where it happens
+    def generate_offsets(self):
+        '''Returns an array of n, 2d arrays that describes how each layer must change'''
+    def get_wing(self):
+        '''Returns n, 2d arrays that descirbe the foil'''
+        #Compute linear offsets
 
 
 class Foil():
-    def __init__(self, points):
+    def __init__(self, points, chord):
+        self.Surface = self.resample(points,1000) #incoming foils are resampled
+        self.Chord = chord
+        self.angle = 0
+    def resample(self, surface_,  samples):
+        '''resamples the foil at a higher density'''
+        self.x_points = surface_[:,0] #Extracts an array of all x points
+        self.y_points = surface_[:,1] #Extracts an array of all y points
+        self.x_points = r_[self.x_points,self.x_points[0]] # Adds the first x point to the end to form a closed curve
+        self.y_points = r_[self.y_points,self.y_points[0]] # Adds the first y point to the end to form a closed curve
+        self.tck , self.u = interpolate.splprep([self.x_points,self.y_points],k=5,s=0.1,per=True) #From a parameterised interpolation of the points given
+        self.i_x, self.i_y = interpolate.splev(linspace(0,1,samples),self.tck) # samples n points along the interpolation
+        self.resampled_foil = stack((self.i_x,self.i_y),axis=1) #Stacks all the individual samples in to 1 2d array
+        return self.resampled_foil
+    def cart2polar(self, point):
+        '''Converts from cartesian to polar'''
+        self.rho = np.sqrt(point[0]**2 + point[1]**2)
+        self.phi = np.arctan2(point[1], point[0])
+        return [self.rho, self.phi]
+    def pol2cart(rho, phi):
+        '''Converts from polar to cartesian'''
+        self.x = rho * np.cos(phi)
+        self.y = rho * np.sin(phi)
+        return [self.x, self.y]
+    def curve_model(self,x, a, b, c, d):
+        '''Defines the model used during the curve fitting step'''
+        return (a*x**3)+(b*x**2)+(c*x)+d
+    def find_chamber_point(self, point):
+        '''Find cartesian location of a point a percentage of a way along a foils chamber line using NLSR'''
+        self.Coefficients, _ = curve_fit(self.curve_model, self.Surface[:,0], self.Surface[:,1], p0 = [1, 1, 1, 1]) #Computes the optimum Coefficients to fit the curve model to the foils points using NLSS
+        self.a, self.b, self.c, self.d = self.Coefficients
+        self.Sample_points = [[x/1000, self.curve_model(x/1000,self.a, self.b, self.c, self.d)] for x in range(0,1000)]
+        self.Steps = np.diff(self.Sample_points)
+        self.Step_lengths = np.hypot(self.Steps[:,0], self.Steps[:,1])
+        self.Chamber_Length = np.sum(self.Step_lengths) # This is the total length of the chamber line
+        self.chamber_point_length = point*self.Chamber_Length
+        self.integrator = 0
+        i=0
+        while self.integrator<self.chamber_point_length:
+            self.integrator = self.integrator +self.Step_lengths[i]
+            i+=1
+        self.chamber_point = [i/1000, self.curve_model(i/1000, self.a, self.b, self.c, self.d)]
+        return self.chamber_point
+    def set_angle(self, axis = 0.25):
+        '''Rotates the foil about a given point on the chord line'''
+        self.axis = self.find_chamber_point(axis) #Compute chord line
+        #Rotates each point about the selected point
+        self.working_foil = []
+        for self.point in self.Surface:
+            self.offest_point = self.point - self.axis
+            self.polar_point = self.cart2polar(self.offest_point)
+            self.polar_point +=[0,self.angle]
+            self.new_point = self.pol2cart(self.polar_point)
+            self.working_foil.append(self.new_point)
+        return self.working_foil +self.axis
+    def get_foil(self):
+        '''returns a list of points that describe the foil'''
+        self.delivered_foil = self.set_angle() #Rotate foil
+        self.delivered_foil *= self.Chord      #Scale foil
+        return self.delivered_foil             #return foil
+
+class Slicer():
+    def __init__(self):
+        self.Layer_height = 0.1
 
 
 
@@ -32,7 +102,7 @@ class main():
         self.Load_airframe_defintion()
 
     def Load_airframe_defintion(self):
-        """XFLR5 Stores its airfract definitions a as XML file"""
+        """XFLR5 Stores its aircraft definitions a as XML file"""
 
         #Waiting on response from forum
         pass
